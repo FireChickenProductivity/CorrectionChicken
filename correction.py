96e737511e9c6585746dd7f2534ba534c3e8c154
Fire Chicken <firechickenproductivity@gmail.com>
2025-01-03 16:25:50 -0700

    Try to preserve original formatting

--- correction.py
+++ correction.py
@@ -5,6 +5,36 @@ last_phrase = ""
 phrase_numbering = ""
 words = []
 
+class Casing:
+    UPPERCASE = 1
+    LOWERCASE = 2
+    CAPITALIZED = 3
+    OTHER = 4
+    def __init__(self, word: str):
+        self.word = word
+        if word.islower():
+            self.casing = Casing.LOWERCASE
+        elif word.isupper():
+            if len(word) == 1:
+                self.casing = Casing.CAPITALIZED
+            else:
+                self.casing = Casing.UPPERCASE
+        elif word[0].isupper():
+            self.casing = Casing.CAPITALIZED
+        else:
+            self.casing = Casing.OTHER
+    
+    def convert(self, word: str):
+        if self.casing == Casing.UPPERCASE:
+            return word.upper()
+        elif self.casing == Casing.LOWERCASE:
+            return word.lower()
+        elif self.casing == Casing.CAPITALIZED:
+            return word.capitalize()
+        else:
+            return word
+
+
 module = Module()
 @module.action_class
 class Actions:
@@ -45,6 +75,14 @@ class Actions:
         global last_phrase
         actions.user.correction_chicken_replace_text(" ".join(words))
 
+    def correction_chicken_replace_word_with_same_casing(index: str, replacement: str):
+        """Replace the specified word with the specified replacement using the same casing"""
+        global words
+        word = words[index - 1]
+        casing = Casing(word)
+        words[index - 1] = casing.convert(replacement)
+        actions.user.correction_chicken_replace_text_with_words(words)
+
     def correction_chicken_remove_characters_from_word(word_number: int, characters: int):
         """Remove the specified number of characters from the specified word"""
         global words
@@ -75,14 +113,12 @@ class Actions:
     def correction_chicken_homophones_advance_word(word_number: int):
         """Advance the word to the next homophone"""
         global words
-        word = words[word_number - 1]
+        word = words[word_number - 1].lower()
         homophones = actions.user.correction_chicken_get_homophones(word)
-        print('homophones', actions.user.homophones_get(word), word)
         if homophones:
             index = homophones.index(word)
             index = (index + 1) % len(homophones)
-            words[word_number - 1] = homophones[index]
-            actions.user.correction_chicken_replace_text_with_words(words)
+            actions.user.correction_chicken_replace_word_with_same_casing(word_number, homophones[index])
         
     def correction_chicken_get_homophones_other_than_word(word: str):
         """Get the homophones for the specified word other than the word itself"""
@@ -95,12 +131,10 @@ class Actions:
         global words
         word = words[word_number - 1]
         homophones = actions.user.correction_chicken_get_homophones_other_than_word(word)
-        print('homophones', homophones)
         if homophones:
             occurrences = [homophone.count(character) for homophone in homophones]
             index = occurrences.index(max(occurrences))
-            words[word_number - 1] = homophones[index]
-            actions.user.correction_chicken_replace_text_with_words(words)
+            actions.user.correction_chicken_replace_word_with_same_casing(word_number, homophones[index])
 
     def correction_chicken_change_word_to_homophone_containing_characters(word_number: int, characters: List[str]):
         """Change the word to the homophone containing the specified characters"""
@@ -108,12 +142,10 @@ class Actions:
         word = words[word_number - 1]
         homophones = actions.user.correction_chicken_get_homophones_other_than_word(word)
         sub_string = "".join(characters)
-        print('homophones', homophones, 'sub_string', sub_string)
         if homophones:
             for homophone in homophones:
                 if sub_string in homophone:
-                    words[word_number - 1] = homophone
-                    actions.user.correction_chicken_replace_text_with_words(words)
+                    actions.user.correction_chicken_replace_word_with_same_casing(word_number, homophone)
                     break
     
 @imgui.open(y=0)
